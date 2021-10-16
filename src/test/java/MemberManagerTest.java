f15d8fcf3dc70d2497308ef1bcfec5e0b9880a57
karimzakir02 <zakirkarim02@gmail.com>
2021-10-16 19:26:48 -0400

    Integrate MemberManagerTest into the app by setting appropriate parameters

--- src/test/java/MemberManagerTest.java
+++ src/test/java/MemberManagerTest.java
@@ -2,6 +2,8 @@ import league_member.LeagueMemberManager;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class MemberManagerTest {
@@ -15,8 +17,10 @@ public class MemberManagerTest {
     // TODO: Break up these tests. Most rely on multiple methods.
 
     @Test
-    public void testAddMember() {
-        String[] addMemberCommand = {"add_member", "Paul Gries"};
+    public void testAddMember() throws Exception {
+        ArrayList<String> addMemberCommand = new ArrayList<String>();
+        addMemberCommand.add("add_member");
+        addMemberCommand.add("Paul Gries");
         assertEquals(m.execute(addMemberCommand),
                 "New Member Paul Gries with 0 correct bet(s) and " +
                         "0 incorrect bet(s) successfully added.");
@@ -27,44 +31,53 @@ public class MemberManagerTest {
         } catch (Exception e) {
             assertEquals(e.getMessage(), "Paul Gries is already an existing Member!");
         }
-
-        String[] memberInfoCommand = {"member_info", "Paul Gries"};
+        ArrayList<String> memberInfoCommand = new ArrayList<String>();
+        memberInfoCommand.add("member_info");
+        memberInfoCommand.add("Paul Gries");
         assertEquals(m.execute(memberInfoCommand),
                 "Member Paul Gries with 0 correct bet(s) and " +
                         "0 incorrect bet(s)");
     }
 
     @Test
-    public void testCreateMatch() {
-        String[] createMatchCommand = {"create_match", "Superb Owl",
-                "Ravens", "Owls"};
+    public void testCreateMatch() throws Exception {
+        ArrayList<String> createMatchCommand = new ArrayList<String>();
+        createMatchCommand.add("create_match");
+        createMatchCommand.add("Super Bowl");
+        createMatchCommand.add("Ravens");
+        createMatchCommand.add("Owls");
         // TODO: Update this to reflect Match.toString()
         assertEquals(m.execute(createMatchCommand),
-                "New Match Superb Owl successfully created.");
+                "New Match Super Bowl successfully created.");
 
         try {
             String failMessage = m.execute(createMatchCommand);
             fail();
         } catch (Exception e) {
-            assertEquals(e.getMessage(), "Superb Owl is already an existing Match!");
+            assertEquals(e.getMessage(), "Super Bowl is already an existing Match!");
         }
     }
 
     @Test
-    public void testBet() {
+    public void testBet() throws Exception {
         testAddMember();
 
         testCreateMatch();
-
-        String[] betCommand = {"bet", "Paul Gries",
-                "Superb Owl", "Owls"};
+        ArrayList<String> betCommand = new ArrayList<String>();
+        betCommand.add("bet");
+        betCommand.add("Paul Gries");
+        betCommand.add("Super Bowl");
+        betCommand.add("Owls");
         assertEquals(m.execute(betCommand),
                 "Paul Gries has successfully placed a bet that " +
-                        "Owls will win Superb Owl");
+                        "Owls will win Super Bowl");
 
         try {
-            String[] betFakeMemberCommand = {"bet", "Gries Paul",
-                    "Superb Owl", "Owls"};
+            ArrayList<String> betFakeMemberCommand = new ArrayList<String>();
+            betFakeMemberCommand.add("bet");
+            betFakeMemberCommand.add("Gries Paul");
+            betFakeMemberCommand.add("Super Bowl");
+            betFakeMemberCommand.add("Owls");
             String failMessage = m.execute(betFakeMemberCommand);
             fail();
         } catch (Exception e) {
@@ -73,8 +86,11 @@ public class MemberManagerTest {
         }
 
         try {
-            String[] betFakeMatchCommand = {"bet", "Paul Gries",
-                    "Super Bowl", "Owls"};
+            ArrayList<String> betFakeMatchCommand = new ArrayList<>();
+            betFakeMatchCommand.add("bet");
+            betFakeMatchCommand.add("Paul Gries");
+            betFakeMatchCommand.add("Superb Owl");
+            betFakeMatchCommand.add("Owls");
             String failMessage = m.execute(betFakeMatchCommand);
             fail();
         } catch (Exception e) {
@@ -82,8 +98,11 @@ public class MemberManagerTest {
         }
 
         try {
-            String[] betFakeTeamCommand = {"bet", "Paul Gries",
-                    "Super Bowl", "Dogs"};
+            ArrayList<String> betFakeTeamCommand = new ArrayList<String>();
+            betFakeTeamCommand.add("bet");
+            betFakeTeamCommand.add("Paul Gries");
+            betFakeTeamCommand.add("Super Bowl");
+            betFakeTeamCommand.add("Dogs");
             String failMessage = m.execute(betFakeTeamCommand);
             fail();
         } catch (Exception e) {
@@ -93,10 +112,13 @@ public class MemberManagerTest {
     }
 
     @Test
-    public void testResolveMatch() {
+    public void testResolveMatch() throws Exception {
         testBet();
 
-        String[] resolveMatchCommand = {"resolve_match", "Superb Owl", "Owls"};
+        ArrayList<String> resolveMatchCommand = new ArrayList<String>();
+        resolveMatchCommand.add("resolve_match");
+        resolveMatchCommand.add("Superb Owl");
+        resolveMatchCommand.add("Owls");
         assertEquals(m.execute(resolveMatchCommand),
                 "Superb Owl was successfully resolved with Owls " +
                         "as the Victors.");
@@ -108,7 +130,9 @@ public class MemberManagerTest {
             assertEquals(e.getMessage(), "Superb Owl is not an existing Match!");
         }
 
-        String[] memberInfoCommand = {"member_info", "Paul Gries"};
+        ArrayList<String> memberInfoCommand = new ArrayList<String>();
+        memberInfoCommand.add("member_info");
+        memberInfoCommand.add("Paul Gries");
         assertEquals(m.execute(memberInfoCommand),
                 "Member Paul Gries with 1 correct bet(s) and " +
                         "0 incorrect bet(s)");
