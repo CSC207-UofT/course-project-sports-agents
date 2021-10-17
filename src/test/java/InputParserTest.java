24a404fc3260d8962275a703a1bed07894866f79
karimzakir02 <zakirkarim02@gmail.com>
2021-10-16 23:18:39 -0400

    Add tests for InputParser class

---
+++ src/test/java/InputParserTest.java
@@ -0,0 +1,30 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import SportsApp.inputParser;
+
+import java.util.ArrayList;
+
+public class InputParserTest {
+
+    private inputParser parser;
+
+    @Test
+    public void memberManagerTest() {
+        inputParser parser = new inputParser("member_manager add_member \"Paul Gries\"");
+        assertEquals(parser.getKeyword(), "member_manager");
+        ArrayList<String> arguments = new ArrayList<>();
+        arguments.add("add_member");
+        arguments.add("Paul Gries");
+        assertEquals(parser.getArguments(), arguments);
+    }
+
+    @Test public void statsPlayerTest() {
+        inputParser parser = new inputParser("stats_player \"Mickael Mendez\" \"lessons TA'd\" \"20202021\"");
+        assertEquals(parser.getKeyword(), "stats_player");
+        ArrayList<String> arguments = new ArrayList<>();
+        arguments.add("Mickael Mendez");
+        arguments.add("lessons TA'd");
+        arguments.add("20202021");
+        assertEquals(parser.getArguments(), arguments);
+    }
+}
