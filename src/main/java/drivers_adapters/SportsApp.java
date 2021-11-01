71264712e2f85ed55e5f1a664b62296c5db234ac
karimzakir02 <zakirkarim02@gmail.com>
2021-10-31 21:59:33 -0400

    Add sport as argument to certain commands

--- src/main/java/drivers_adapters/SportsApp.java
+++ src/main/java/drivers_adapters/SportsApp.java
@@ -8,11 +8,11 @@ public class SportsApp {
         System.out.println("Hi! Welcome to our sports agent app");
         System.out.println("Currently, the following commands are available:");
         System.out.println("member_manager add_member \"member_name\": adds a new member for your fantasy league");
-        System.out.println("member_manager create_match \"match_name\" \"team1_name\" \"team2_name\": create a match");
-        System.out.println("member_manager bet \"member_name\" \"match_name\" \"winner_name\": make a bet on a match on the behalf of a player");
-        System.out.println("member_manager resolve_match \"match_name\" \"winning_team_name\": end a match, declare one team won, and update members based on bets");
+        System.out.println("member_manager create_match \"sport_name\" \"match_name\" \"team1_name\" \"team2_name\": create a match");
+        System.out.println("member_manager bet \"sport_name\" \"member_name\" \"match_name\" \"winner_name\": make a bet on a match on the behalf of a player");
+        System.out.println("member_manager resolve_match \"sport_name\" \"match_name\" \"winning_team_name\": end a match, declare one team won, and update members based on bets");
         System.out.println("member_manager member_info \"member_name\": return information on a member");
-        System.out.println("stats_player \"player_name\" \"stat\" \"seasons\": get available data on a player of your choosing");
+        System.out.println("stats_player \"sport_name\" \"player_name\" \"stat\" \"seasons\": get available data on a player of your choosing");
         System.out.println("Examples of available players: Auston Matthews, Brad Marchand, Kirill Kaprizov");
         System.out.println("Examples of available statistics: games played, assists, shots, all stats");
         System.out.println("Example of a season: 20202021");
