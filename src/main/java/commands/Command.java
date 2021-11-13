c55564196d68915dc3faa679440cf3e2379d8b64
karimzakir02 <zakirkarim02@gmail.com>
2021-11-13 12:45:51 -0500

    Fix code style isssue in Command.java

--- src/main/java/commands/Command.java
+++ src/main/java/commands/Command.java
@@ -4,6 +4,6 @@ import java.util.ArrayList;
 
 public interface Command {
 
-    public String execute(ArrayList<String> arguments) throws Exception;
+    String execute(ArrayList<String> arguments) throws Exception;
 
 }
