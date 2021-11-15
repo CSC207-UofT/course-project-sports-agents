f65041bce5652142cbd5b9944b43dd9e1854df12
karimzakir02 <zakirkarim02@gmail.com>
2021-11-15 12:30:27 -0500

    Add LeagueStorage and appropriate methods

--- src/main/java/commands/LeagueMemberManager.java
+++ src/main/java/commands/LeagueMemberManager.java
@@ -3,16 +3,18 @@ package commands;
 import constants.Exceptions;
 import drivers_adapters.DataContainer;
 import leagueMember.LeagueMember;
+import leagueMember.LeagueStorage;
 import match.Match;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.ArrayList;
 
 public class LeagueMemberManager implements Command {
     // MemberMap stores the League Members of the fantasy league
-    public final HashMap<String, LeagueMember> LeagueMemberMap;
+    public HashMap<String, LeagueMember> LeagueMemberMap;
     // GameMap stores the ongoing Games of the fantasy league
-    public final HashMap<String, Match> MatchMap;
+    public HashMap<String, Match> MatchMap;
 
     /**
      * Create a new MemberManager with no Members and no Games
@@ -34,10 +36,54 @@ public class LeagueMemberManager implements Command {
                 return resolveMatch(arguments);
             case "member_info":
                 return memberInfo(arguments);
+            case "save":
+                return saveLeague(arguments);
+            case "load":
+                return loadLeague(arguments);
             default:
                 throw new Exception(Exceptions.WRONG_COMMAND);
         }
     }
+
+    private String loadLeague(ArrayList<String> arguments) {
+        String path = arguments.get(1);
+        try {
+            ObjectInputStream in = new ObjectInputStream(new FileInputStream(path));
+            LeagueStorage loaded = (LeagueStorage) in.readObject();
+
+            this.LeagueMemberMap = loaded.LeagueMemberMap;
+            this.MatchMap = loaded.MatchMap;
+
+            in.close();
+
+            return "Successfully loaded the league. Welcome back!";
+
+        } catch (FileNotFoundException e) {
+            return "Could not find the given path";
+        } catch (IOException | ClassNotFoundException e) {
+            return "Could not load the given path";
+        }
+    }
+
+    private String saveLeague(ArrayList<String> arguments) throws Exception {
+        String path = arguments.get(1);
+        LeagueStorage to_save = new LeagueStorage(LeagueMemberMap, MatchMap);
+
+        try {
+            FileOutputStream fout = new FileOutputStream(path);
+            ObjectOutputStream out = new ObjectOutputStream(fout);
+
+            out.writeObject(to_save);
+            out.flush();
+            out.close();
+
+            return "Successfully saved to " + path;
+        }
+        catch (Exception e) {
+            throw new Exception("Could not save to " + path);
+        }
+    }
+
 
     /**
      * Add a new League Member to the fantasy league
