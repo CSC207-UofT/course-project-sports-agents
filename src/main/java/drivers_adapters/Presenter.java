6e3e151af912a3d572467f6b981f72bffce6ba1d
Isaiah Hazelwood <isaiah.hazelwood@mail.utoronto.ca>
2021-11-22 12:42:23 -0500

    Added Javadoc

--- src/main/java/drivers_adapters/Presenter.java
+++ src/main/java/drivers_adapters/Presenter.java
@@ -2,10 +2,22 @@ package drivers_adapters;
 
 public interface Presenter {
 
+    /**
+     * Prompt the User for an input
+     * @return the User's input command
+     */
     String getInput();
 
+    /**
+     * Display the given output to the User
+     * @param output output to display
+     */
     void presentOutput(String output);
 
+    /**
+     * Close the output stream.
+     * Called when the program quits.
+     */
     void closeOutput();
 
 }
