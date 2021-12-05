13a35e5c4122abc262d63a045a58287ce88473a2
nazzaninghazzi <90343958+nazzaninghazzi@users.noreply.github.com>
2021-12-04 20:02:35 -0500

    Made GetList and Search implement Command interface + Added some methods for getting all players in a specific sport and season in CSVDataContainer.

--- src/main/java/commands/GetList.java
+++ src/main/java/commands/GetList.java
@@ -2,6 +2,8 @@ package commands;
 
 
 import constants.*;
+import drivers_adapters.*;
+import player.*;
 
 import java.io.*;
 import java.util.*;
@@ -10,74 +12,56 @@ import java.util.*;
  * Return a list of available sports, valid stats for each sport, and available users for each
  * season and sport so that the user will have a background of the program.
  */
-public class GetList {
+public class GetList implements Command{
 
-    public List<String> getAllSports(){
-        return (Arrays.asList("Baseball", "Hockey", "Tennis"));
+    public String execute(){
+        return "All Available sports: \n" + "Baseball\n" +
+                "Hockey\n" +
+                "Tennis\n";
     }
 
-    public List<String> getAllStat(String sportName) throws Exception {
+    public String execute(String sportName) throws Exception {
         switch (sportName.toLowerCase()){
             case "hockey":
-                return Arrays.asList("Team", "Skater Shoots",
-                        "Position", "Games Played", "Goals", "Assists",
-                        "Points", "Shots", "Shooting Percentage");
+                return "All available Stats For " + sportName+ "\n"+
+                        "Team\n" + "Skater Shoots\n" + "Position\n" + "Games Played\n" +
+                        "Goals\n" +"Assists\n" + "Points\n"+ "Shots\n"+ "Shooting Percentage\n";
+
             case "baseball":
-                return Arrays.asList("Team",
-                        "Position", "Games Played", "At Bats", "Runs",
-                        "Hits", "Home Runs", "Runs Batted In", "Strike Outs", "Average");
+                return "All available Stats For " + sportName+ "\n"+
+                        "Team\n" + "Position\n" + "Games Played\n" + "At Bats\n" +
+                        "Runs\n" +"Hits\n" + "Home Runs\n"+ "Runs Batted In\n"+ "Strike Outs\n"+
+                        "Average\n";
+
             case "tennis":
-                return Arrays.asList("Country", "Rank", "Matches", "Aces", "Double Faults", "Serve Points Won",
-                        "Break Points Saved", "Serve Games Won", "Return Games Won", "Break Points Converted",
-                        "Return Points Won");
+                return "All available Stats For " + sportName+ "\n"+
+                        "Rank\n" + "Matches n\n" + "Aces\n" + "Double Faults\n" +
+                        "Serve Points Won\n" +"Breakpoints Saved\n" + "Serve Games Won\n"+
+                        "Return Games Won\n"+ " Break Points Converted\n" +
+                        "Return Points Won\n";
         }
         throw new Exception(Exceptions.WRONG_SPORT);
     }
 
-    public List<String> getAllPlayers(String sportName, String season) throws Exception {
-        switch (sportName.toLowerCase()){
-            case "hockey":
-                return getAllHockeyPlayers(season);
-            case "baseball":
-                return getAllBaseballPlayers(season);
-            case "tennis":
-                return null;
-        }
-        throw new Exception(Exceptions.WRONG_SPORT);
+    public String execute(ArrayList<String> arguments, DataContainer container) throws Exception {
+        String sportName = arguments.get(0);
+        String season = arguments.get(1);
+        List<String> listAllPlayers =  container.getAllPlayers(sportName, season);
+        return formatGetListPlayers(listAllPlayers);
     }
 
-    public List<String> getAllHockeyPlayers(String season) throws Exception {
-        List<String> allPlayers = new ArrayList<>();
-        BufferedReader br = new BufferedReader(new FileReader("hockey.csv"));
-        br.readLine(); //skip the first line.
-        String line;
-        String splitBy = ",";
-
-        while((line = br.readLine()) != null) {
-            String[] playerInfo = line.split(splitBy);
-            if (playerInfo[1].equals(season)){
-                allPlayers.add(playerInfo[0]);
+    private String formatGetListPlayers(List<String> listAllPlayers) {
+        StringBuilder output = new StringBuilder();
+        output.append("All available players for the demanded sport and season:\n");
+        for (int i = 0; i < listAllPlayers.size(); i++){
+            if(i % 10 == 0){
+                output.append("\n");
             }
+            output.append(listAllPlayers.get(i));
+            output.append(", ");
+
         }
-        return allPlayers;
-
-    }
-
-    public List<String> getAllBaseballPlayers(String season) throws Exception {
-        List<String> allPlayers = new ArrayList<>();
-        BufferedReader br = new BufferedReader(new FileReader("baseball.csv"));
-        br.readLine(); //skip the first line.
-        String line;
-        String splitBy = ",";
-
-        while((line = br.readLine()) != null) {
-            String[] playerInfo = line.split(splitBy);
-            if (playerInfo[2].equals(season)){
-                allPlayers.add(playerInfo[0]);
-            }
-        }
-        return allPlayers;
-
+        return output.toString();
     }
 
 
